ac9fc892315097c030e232d4369fa762c84aeb4f
botprzemek <botprzemekk@gmail.com>
2023-11-29 09:54:32 +0100

    Updated README.md

--- services/storage/method/filter.method.ts
+++ services/storage/method/filter.method.ts
@@ -3,6 +3,7 @@ import * as cityFilter from 'services/storage/method/filter/city.filter'
 import * as fundFilter from 'services/storage/method/filter/fund.filter'
 import * as playerFilter from 'services/storage/method/filter/player.filter'
 import * as playerStatisticsFilter from 'services/storage/method/filter/playerStatistics.filter'
+import * as rosterFilter from 'services/storage/method/filter/roster.filter'
 import * as staffFilter from 'services/storage/method/filter/staff.filter'
 import * as teamFilter from 'services/storage/method/filter/team.filter'
 import * as leagueFilter from 'services/storage/method/filter/league.filter'
@@ -14,6 +15,7 @@ export default {
 	...fundFilter,
 	...playerFilter,
 	...playerStatisticsFilter,
+	...rosterFilter,
 	...staffFilter,
 	...teamFilter,
 	...leagueFilter,
