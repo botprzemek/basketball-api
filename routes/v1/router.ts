e406ce12baa993cdf92c651f71022e9b7cdb8e6b
botprzemek <info@botprzemek.pl>
2023-12-29 00:39:32 +0100

    Refactor

--- routes/v1/router.ts
+++ routes/v1/router.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import error from 'middlewares/error.middleware'
 import secret from 'middlewares/key.middleware'
-import sendMiddleware from 'middlewares/send.middleware'
+import send from 'middlewares/send.middleware'
 import getRouter from 'routes/v1/router/route.router'
 import authRoute from 'routes/v1/router/route/auth.route'
 import routes from 'utils/route.util'
@@ -14,6 +14,6 @@ router.use('/auth', authRoute)
 
 Object.keys(routes).forEach((route: string) => router.use(`/${route}`, getRouter(route)))
 
-router.use(sendMiddleware)
+router.use(send)
 
 export default router
