de26f941d2954c235f22cc1cd6c21a5df10979fa
botprzemek <info@botprzemek.pl>
2024-10-29 14:35:58 +0100

    feat(api): implementing cluster mode for web server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,28 +1,28 @@
 import logger from "@/utils/logger";
 import router from "@/server/router";
-import { getAddress } from "@/config/server";
+import { getAddress, getVersion } from "@/config/server";
 
 import { createServer, Server as HttpServer } from "node:http";
+import cluster from "node:cluster";
+import os from "os";
 
-const api: HttpServer = createServer(router);
-
-const close = (): void => {
+const close = (server: HttpServer): void => {
     logger.info(getAddress().host, ["LISTEN", "Closing API server"]);
 
-    api.close((err: Error | undefined): void => {
+    server.close((err: Error | undefined): void => {
         logger.info(getAddress().host, ["LISTEN", "Stopped API server"]);
 
         process.exit(err ? 1 : 0);
     });
 };
 
-export const listen = (server: HttpServer = api): void => {
+export const listen = (server: HttpServer): void => {
     if (server.listening) {
         return;
     }
 
-    process.on("SIGINT", close);
-    process.on("SIGTERM", close);
+    process.on("SIGINT", () => close(server));
+    process.on("SIGTERM", () => close(server));
     process.on("uncaughtException", (error: Error): void => {
         logger.error(getAddress().host, [error.stack as string]);
         // TODO
@@ -32,9 +32,25 @@ export const listen = (server: HttpServer = api): void => {
     server.listen(getAddress().port, getAddress().host, () =>
         logger.info(getAddress().host, [
             "LISTEN",
-            `Started API server on port ${getAddress().port}`,
+            `Started API server on http://${getAddress().host}:${getAddress().port}/v${getVersion()}`,
         ]),
     );
 };
 
-export default listen;
+export default (): void => {
+    if (!cluster.isPrimary) {
+        const server = createServer(router);
+
+        server.keepAliveTimeout = 65000;
+        server.headersTimeout = 65000;
+        server.maxHeadersCount = 1000;
+
+        return listen(server);
+    }
+
+    for (let i = 0; i < os.cpus().length; i++) {
+        cluster.fork();
+    }
+
+    cluster.on("exit", (_) => cluster.fork());
+};
